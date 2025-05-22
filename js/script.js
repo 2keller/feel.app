30da0b26dc0b37cc046ddd944cc5e778e2fac33c
2keller <96345215+2keller@users.noreply.github.com>
2025-05-22 14:14:02 +0000

    feels first iteration

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Currently, no specific JavaScript is needed for the homepage layout.
+// This file will be used for interactive elements on other pages or
+// for future enhancements on the homepage itself.
+
+// Example: Simple console log to confirm script is loaded
+console.log("Feel Flow scripts loaded!");
